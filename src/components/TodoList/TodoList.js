778b6f6655aa6e53c17ecdb916a4b44bc51f1451
Kaia Olson <kaia@junglescout.com>
2020-09-14 09:52:05 -0700

    Use usePromise instead of local state for TodoList.

--- src/components/TodoList/TodoList.js
+++ src/components/TodoList/TodoList.js
@@ -1,43 +1,17 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import { Link } from 'react-router-dom'
 import { fetchTodos } from '../../api/fetchTodos'
+import { usePromise } from '../../hooks/usePromise'
 
 export const TodoList = () => {
-  const [data, setData] = useState([])
-  const [loading, setLoading] = useState(true)
-  const [error, setError] = useState(null)
-
-  useEffect(() => {
-    fetchTodos()
-      .then(res => {
-        setData(res)
-        setLoading(false)
-      })
-      .catch(error => {
-        setError(error)
-        setLoading(false)
-      })
-  }, [])
+  const { data, loading, error, execute } = usePromise(() => fetchTodos(), [])
 
   if (error) {
     return (
       <h1>
         There was an error.
         <br />
-        <button
-          onClick={() => {
-            fetchTodos()
-              .then(res => {
-                setData(res)
-                setLoading(false)
-              })
-              .catch(error => {
-                setError(error)
-                setLoading(false)
-              })
-          }}>
-          Try again
-        </button>
+        <button onClick={execute}>Try again</button>
       </h1>
     )
   }
